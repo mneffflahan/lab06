ac59dd25bbb35ca7d872706185e21cf5a67abca6
heimanj <jasonheiman7@gmail.com>
2023-03-21 19:14:50 -0400

    Added decode function for usage

--- main.py
+++ main.py
@@ -15,6 +15,22 @@ def encode_one(x):
         x -= 7
     return str(x)
 
+def decode(encoded_password):
+    encoded_list = list(encoded_password)
+    for x in range(len(encoded_list)):
+        if encoded_list[x] == "0":
+            encoded_list[x] = "7"
+        elif encoded_list[x] == "1":
+            encoded_list[x] = "8"
+        elif encoded_list[x] == "2":
+            encoded_list = "9"
+        else:
+            encoded_list[x] = int(encoded_list[x])
+            encoded_list[x] = int(encoded_list[x] - 3)
+            encoded_list[x] = str(encoded_list[x])
+    password = ''.join(encoded_list)
+    return password
+
 
 def menu():
     print("Menu")
